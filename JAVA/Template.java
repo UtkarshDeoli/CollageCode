a1c1b91eb733db8153a2464b9394d5c33bc3e3fe
Utkarsh Deoli <utkarsh.deoli@gmail.com>
2023-02-25 14:22:33 +0530

    25-02-2023

---
+++ JAVA/Template.java
@@ -0,0 +1,15 @@
+class Test
+{
+    public static void printX()
+    {
+        System.out.println('$');
+    }
+}
+
+public class Template
+{
+    public static void main(String [] args)
+    {
+        Test.printX();
+    }
+}
