6aac59690fb1fac1fec769b3dd15e5103d58e6c2
UtkarshDeoli <utkarsh.deoli@gmail.com>
2024-03-15 09:18:42 +0530

    ^_^

--- WebDev/class2/main.js
+++ WebDev/class2/main.js
@@ -1,20 +1,45 @@
-var colorChange;
-var times = 0;
+var timerfuntion;
+var hour =0;
+var minutes=0;
+var seconds=0;
 var isTimerRunning = false;
 
 function startTimer() {
-    colorChange = setTimeout(addSecond, 1000);
+    timerfuntion = setInterval(addSecond, 1000);//setTimeout()
 }
 
 function stopTimer() {
-    clearTimeout(colorChange);
+    clearInterval(timerfuntion);//clearTimeout()
     times = 0;
+    document.getElementById('sec').innerText=seconds;
     isTimerRunning = false;
 }
 
 function addSecond()
 {
-    times=times+1;
-    document.getElementById('time').innerText=times;
-    startTimer();
+    seconds++;
+    if(seconds==60)
+    {
+        seconds=0;
+        minutes++;
+        document.getElementById('minutes').innerText=minutes.toString().padStart(2, '0');
+    }
+    if(minutes==60)
+    {
+        minutes=0;
+        document.getElementById('minutes').innerText=minutes.toString().padStart(2, '0');
+        hour++;
+        document.getElementById('hour').innerText=hour.toString().padStart(2, '0');
+    }
+    document.getElementById('sec').innerText=seconds.toString().padStart(2, '0');
+    //call s
+}
+
+
+function getID()
+{
+    var fromUnit = document.getElementById('from').value;
+    var toUnit = document.getElementById('to').value;
+
+
 }
